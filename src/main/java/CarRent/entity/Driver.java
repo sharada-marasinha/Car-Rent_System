a8fc37230013baa6e25d57b90044171927cd6dc2
Sharada022 <sharadanuwanbandara0765256752@gmail.com>
2022-07-10 20:59:08 +0530

    Update Driver

--- src/main/java/CarRent/entity/Driver.java
+++ src/main/java/CarRent/entity/Driver.java
@@ -1,4 +1,25 @@
 package CarRent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Blob;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@ToString
 public class Driver {
+    @Id
+    private String driID;
+    private String driName;
+    private String driAddress;
+    private String driContact;
+    private String drilicence;
+
 }
